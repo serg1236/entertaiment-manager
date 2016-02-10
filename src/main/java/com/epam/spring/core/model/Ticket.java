c80c1fc3a94402926d835fcdf0304b27e1273719
serg1236 <seryoga.dah@gmail.com>
2016-02-10 14:03:24 +0200

    Initial commit

---
+++ src/main/java/com/epam/spring/core/model/Ticket.java
@@ -0,0 +1,5 @@
+package com.epam.spring.core.model;
+
+public class Ticket {
+
+}
---
+++ src/main/java/com/epam/spring/core/service/UserService.java
@@ -0,0 +1,17 @@
+package com.epam.spring.core.service;
+
+import java.util.List;
+
+import com.epam.spring.core.model.Ticket;
+import com.epam.spring.core.model.User;
+
+public interface UserService {
+	
+	void register(User user);
+	void remove(User user);
+	User getById(int id);
+	User getUserByEmail(String email);
+	User getUserByName(String name);
+	List<Ticket> getBookedTickets(User user);
+
+}
